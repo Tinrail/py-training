9144363d7136b150776d205e5f5b84f52469ab4f
rey.tin <dir2004@gmail.com>
2020-01-19 09:46:34 +0800

    modify file

--- duty.py
+++ duty.py
@@ -1,22 +1,22 @@
 import calendar
 year = 2020
-base_income_value = 80
-double_income_value = base_income_value * 2
-festival_income_value = 300
-month = input("please enter current month: ")
+base_income_value = 80   #普通日值班费
+double_income_value = base_income_value * 2 #周末值班费
+festival_income_value = 300  #节日值班费
+month = input("please enter current month: ")   #输入要计算那个月的费用
 monthRange = calendar.monthrange(year,int(month))
 mon = {}
 
-festivals = input("please enter festival date: ").split()
+festivals = input("please enter festival date: ").split()  #输入这个月内的节日日期
 festivals = [int(festival) for festival in festivals]
 
-double_income = input("please enter double_income day: ").split()
+double_income = input("please enter double_income day: ").split() #输入因节日产生的双倍值班费日期
 double_income = [int(di) for di in double_income]
 
-cl_we = input("please enter cancel weekend double date: ").split()
+cl_we = input("please enter cancel weekend double date: ").split()  #属于因调整节假日而取消的周末费日期
 cl_we = [int(cw) for cw in cl_we]
 
-if monthRange[0] == 6:
+if monthRange[0] == 6: 
 	week = 0
 else:
 	week = monthRange[0] + 1
@@ -55,10 +55,12 @@ def cal_income(dutys,name):
 			weekday +=1
 	if festivals:
 		print ("")
-		print (name+"  ------>  weekday: "+str(weekday)+"  weekday: "+str(weekend)+"  festivals(300): "+str(spe_day)+"  sum: $"+str(sum))
+		print (name+"  ------>  weekday: "+str(weekday)+"  weekday: "+str(weeken
+			d)+"  festivals(300): "+str(spe_day)+"  sum: $"+str(sum))
 	else:
 		print ("")
-		print (name+"  ------>  weekday: "+str(weekday)+"  weekday: "+str(weekend)+"  sum: $"+str(sum))		
+		print (name+"  ------>  weekday: "+str(weekday)+"  weekday: "+str(weeken
+			d)+"  sum: $"+str(sum))		
 
 while True:
 	user = {}
